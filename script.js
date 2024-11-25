5c6ea7abecd3d42c99587d6c19ae7a2bd2d47ce9
Mariana Candido <mariana.rodrigues.or@gmail.com>
2024-11-25 13:00:42 -0300

    Adicionando modificações do texto confome o clique nos botoes

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-const html = document.querySelector('html');
-const botaoFoco = document.querySelector('.app__card-button--foco')
-const botaoCurto = document.querySelector('.app__card-button--curto')
-const botaoLongo = document.querySelector('.app__card-button--longo')
-const imgCurto = document.querySelector('.app__image')
+const html = document.querySelector("html");
+const botaoFoco = document.querySelector(".app__card-button--foco");
+const botaoCurto = document.querySelector(".app__card-button--curto");
+const botaoLongo = document.querySelector(".app__card-button--longo");
+const img = document.querySelector(".app__image");
+const texto = document.querySelector(".app__title");
 
+botaoFoco.addEventListener("click", () => {
+  mudarContexto("foco");
+  });
 
-botaoFoco.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-    imgCurto.setAttribute('src', './imagens/foco.png')
-})
+botaoCurto.addEventListener("click", () => {
+  mudarContexto("descanso-curto");
+});
 
-botaoCurto.addEventListener('click', () =>{
-    html.setAttribute('data-contexto', 'descanso-curto')
-    imgCurto.setAttribute('src', './imagens/descanso-curto.png')
-})
-botaoLongo.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-    imgCurto.setAttribute('src', './imagens/descanso-longo.png')
-})
+botaoLongo.addEventListener("click", () => {
+  mudarContexto("descanso-longo");
+});
+
+function mudarContexto(contexto) {
+  html.setAttribute("data-contexto", contexto);
+  img.setAttribute("src", `/imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      texto.innerHTML = `<h1 class="app__title">
+          Otimize sua produtividade,<br />
+          <strong class="app__title-strong">mergulhe no que importa.</strong>
+        </h1>`;
+      break;
+    case "descanso-curto":
+      texto.innerHTML = `<h1 class="app__title">
+          Que tal um descanso?<br />
+          <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+      break;
+    case "descanso-longo":
+      texto.innerHTML = `<h1 class="app__title">
+        Hora de voltar a superficie!<br />
+          <strong class="app__title-strong">Faça uma pausa longa!</strong>`;
+      break;
+    default:
+      break;
+  }
+}
