e517d9004d1d265717d36d74c2d3aefe9e19551c
Mariana Candido <mariana.rodrigues.or@gmail.com>
2024-11-27 14:42:33 -0300

    Aplicando localStorage e API JSON

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,17 @@
 const botaoAdicionarTarefa = document.querySelector(".app__button--add-task")
 const formularioAdicionarTarefa = document.querySelector(".app__form-add-task")
+const textoArea = document.querySelector(".app__form-textarea")
+const ListasDetarefas = []
 
 botaoAdicionarTarefa.addEventListener("click", () => {
 formularioAdicionarTarefa.classList.toggle("hidden")
+})
+
+formularioAdicionarTarefa.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+    const tarefa = {descrição: textoArea.value
+     }
+    ListasDetarefas.push(tarefa)
+    localStorage.setItem("tarefa", JSON.stringify(ListasDetarefas))
+    console.table(ListasDetarefas)
 })
